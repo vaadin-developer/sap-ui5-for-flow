52cc0a0a5ad51ba3442c4ebfe8edccfe3f5fee2c
Daniel Nordhoff-Vergien <dve@vergien.net>
2019-10-07 18:07:21 +0200

    start implementing textArea

--- 01_components/src/main/java/org/rapidpm/vaadin/webcomponents/sapui5/UI5TextArea.java
+++ 01_components/src/main/java/org/rapidpm/vaadin/webcomponents/sapui5/UI5TextArea.java
@@ -1,3 +1,24 @@
 package org.rapidpm.vaadin.webcomponents.sapui5;
 
-public class UI5TextArea { }
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.PropertyDescriptor;
+import com.vaadin.flow.component.PropertyDescriptors;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@Tag("ui5-textarea")
+@NpmPackage(value = "@ui5/webcomponents", version = UI5VersionDefinitions.UI5_VERSION)
+@JsModule("@ui5/webcomponents/dist/TextArea.js")
+public class UI5TextArea extends Component implements HasUI5Disabled {
+  static PropertyDescriptor<String, String> placeholderProperty =
+      PropertyDescriptors.propertyWithDefault("placeholder ", "");
+
+  public void setPlaceholder(String placeholder) {
+    placeholderProperty.set(getElement(), placeholder);
+  }
+
+  public String getPlaceholder() {
+    return placeholderProperty.get(getElement());
+  }
+}
