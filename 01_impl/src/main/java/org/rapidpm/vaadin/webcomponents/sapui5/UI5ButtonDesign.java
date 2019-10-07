4ac7f0aeacbabe55f6429a3cdd81112adf83af65
Sven Ruppert <sven.ruppert@gmail.com>
2019-10-07 12:23:27 +0200

    feature/changed_prj_structure

--- 01_impl/src/main/java/org/rapidpm/vaadin/webcomponents/sapui5/UI5ButtonDesign.java
+++ 01_impl/src/main/java/org/rapidpm/vaadin/webcomponents/sapui5/UI5ButtonDesign.java
@@ -3,8 +3,11 @@ package org.rapidpm.vaadin.webcomponents.sapui5;
 import java.util.Arrays;
 
 public enum UI5ButtonDesign {
-  DEFAULT("Default"), EMPHASIZED("Emphasized"), POSITIVE("Positive"), NEGATIVE(
-      "Negative"), TRANSPARENT("Transparent");
+  DEFAULT("Default"),
+  EMPHASIZED("Emphasized"),
+  POSITIVE("Positive"),
+  NEGATIVE("Negative"),
+  TRANSPARENT("Transparent");
 
   private final String attributeName;
 
@@ -17,7 +20,9 @@ public enum UI5ButtonDesign {
   }
 
   public static UI5ButtonDesign fromAttributeName(String attributeName) {
-    return Arrays.stream(UI5ButtonDesign.values()).filter(i -> i.attributeName.equals(attributeName))
-        .findFirst().orElse(null);
+    return Arrays.stream(UI5ButtonDesign.values())
+                 .filter(i -> i.attributeName.equals(attributeName))
+                 .findFirst()
+                 .orElse(null);
   }
 }
--- 03_demo/frontend/styles/shared-styles.js
+++ 03_demo/frontend/styles/shared-styles.js
@@ -1,4 +1,19 @@
-import "@vaadin/vaadin-charts/theme/vaadin-chart-default-theme";
+/*
+ * Copyright © 2017 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// import "@vaadin/vaadin-charts/theme/vaadin-chart-default-theme";
 
 const $_documentContainer = document.createElement('template');
 
@@ -75,44 +90,44 @@ html {
   </template>
 </dom-module>
 
-<dom-module id="chart" theme-for="vaadin-chart">
-  <template>
-    <style include="vaadin-chart-default-theme">
-      :host {
-        --vaadin-charts-color-0: var(--lumo-primary-color);
-        --vaadin-charts-color-1: var(--lumo-error-color);
-        --vaadin-charts-color-2: var(--lumo-success-color);
-        --vaadin-charts-color-3: var(--lumo-contrast);
-      }
-      .highcharts-container {
-        font-family: var(--lumo-font-family);
-      }
-      .highcharts-background {
-        fill: var(--lumo-base-color);
-      }
-      .highcharts-title {
-        fill: var(--lumo-header-text-color);
-        font-size: var(--lumo-font-size-xl);
-        font-weight: 600;
-        line-height: var(--lumo-line-height-xs);
-      }
-      .highcharts-legend-item text {
-        fill: var(--lumo-body-text-color);
-      }
-      .highcharts-axis-title,
-      .highcharts-axis-labels {
-        fill: var(--lumo-secondary-text-color);
-      }
-      .highcharts-axis-line,
-      .highcharts-grid-line,
-      .highcharts-tick {
-        stroke: var(--lumo-contrast-10pct);
-      }
-      .highcharts-column-series rect.highcharts-point {
-        stroke: var(--lumo-base-color);
-      }
-    </style>
-  </template>
-</dom-module>`;
+<!--<dom-module id="chart" theme-for="vaadin-chart">-->
+<!--  <template>-->
+<!--    <style include="vaadin-chart-default-theme">-->
+<!--      :host {-->
+<!--        &#45;&#45;vaadin-charts-color-0: var(&#45;&#45;lumo-primary-color);-->
+<!--        &#45;&#45;vaadin-charts-color-1: var(&#45;&#45;lumo-error-color);-->
+<!--        &#45;&#45;vaadin-charts-color-2: var(&#45;&#45;lumo-success-color);-->
+<!--        &#45;&#45;vaadin-charts-color-3: var(&#45;&#45;lumo-contrast);-->
+<!--      }-->
+<!--      .highcharts-container {-->
+<!--        font-family: var(&#45;&#45;lumo-font-family);-->
+<!--      }-->
+<!--      .highcharts-background {-->
+<!--        fill: var(&#45;&#45;lumo-base-color);-->
+<!--      }-->
+<!--      .highcharts-title {-->
+<!--        fill: var(&#45;&#45;lumo-header-text-color);-->
+<!--        font-size: var(&#45;&#45;lumo-font-size-xl);-->
+<!--        font-weight: 600;-->
+<!--        line-height: var(&#45;&#45;lumo-line-height-xs);-->
+<!--      }-->
+<!--      .highcharts-legend-item text {-->
+<!--        fill: var(&#45;&#45;lumo-body-text-color);-->
+<!--      }-->
+<!--      .highcharts-axis-title,-->
+<!--      .highcharts-axis-labels {-->
+<!--        fill: var(&#45;&#45;lumo-secondary-text-color);-->
+<!--      }-->
+<!--      .highcharts-axis-line,-->
+<!--      .highcharts-grid-line,-->
+<!--      .highcharts-tick {-->
+<!--        stroke: var(&#45;&#45;lumo-contrast-10pct);-->
+<!--      }-->
+<!--      .highcharts-column-series rect.highcharts-point {-->
+<!--        stroke: var(&#45;&#45;lumo-base-color);-->
+<!--      }-->
+<!--    </style>-->
+<!--  </template>-->
+<!--</dom-module>-->`;
 
 document.head.appendChild($_documentContainer.content);
